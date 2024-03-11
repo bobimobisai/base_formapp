0202093eac6cf907c95ed6969b476ef941b708af
Bob Fashan <danilgrachev1992@gmail.com>
2024-03-11 20:37:49 +0300

    f

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.10
+
+
+WORKDIR .
+COPY . .
+
+
+RUN pip3 install --upgrade setuptools
+RUN pip3 install -r  requirements.txt
---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+app = FastAPI()
+templates = Jinja2Templates(directory="src")
+
+
+class Item(BaseModel):
+    name: str
+    description: str
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    print(f"Received item: {item}")
+    return {"message": "Item received"}
+
+
+@app.get("/app", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
