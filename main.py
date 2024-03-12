7b1f97b3b2072a582899b00b0171f279556fbe2e
Bob Fashan <danilgrachev1992@gmail.com>
2024-03-12 23:59:29 +0300

    ookss

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="src")
+app.mount("/images", StaticFiles(directory="/"), name="images")
 
 
 class Item(BaseModel):
@@ -21,3 +24,8 @@ async def create_item(item: Item):
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/img/{image_name}")
+async def get_image(image_name: str):
+    return FileResponse(f"images/{image_name}")
